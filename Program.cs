bd6a3c1702259ef156c575bd1ba504a060a1b8b4
Snnirf <165679497+Snnirf@users.noreply.github.com>
2024-04-02 15:10:47 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Candelaria_Frinna_Number_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int A = 1; A < 101; A++)
+            {
+                if (A % 15 == 0)
+                {
+                    Console.WriteLine("Hello Goodbye");
+                }
+                else if (A % 5 == 0)
+                {
+                    Console.WriteLine("Goodbye");
+                }
+                else if (A % 3 == 0)
+                {
+                    Console.WriteLine("Hello");
+                }
+                else
+                {
+                    Console.WriteLine(A);
+                }
+            }
+        }
+    }
+}
